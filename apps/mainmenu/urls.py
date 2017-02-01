0c92f994cfecbce7878ff050c394455250dabc4e
Brandon Walter <bs.walter91@gmail.com>
2017-01-31 18:32:00 -0800

    added create

--- apps/mainmenu/urls.py
+++ apps/mainmenu/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^create$', views.create, name='create'),
-	url(r'^delete$', views.delete, name='delete'),
-	url(r'^hall$', views.hall, name='hall')
-	
+	url(r'^/$', views.index, name='index'),
+	url(r'^/create$', views.create, name='create'),
+	url(r'^/delete$', views.delete, name='delete'),
+	url(r'^/hall$', views.hall, name='hall'),
+	url(r'^/updatechar$', views.update),
+	url(r'^/roll$', views.roll),
+
 ]
