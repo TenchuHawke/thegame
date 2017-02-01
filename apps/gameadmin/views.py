88014b9c5269c833058f32d4ee2d8363dbcb977a
TenchuHawke <hawke@unrestrainedgames.com>
2017-02-01 10:31:03 -0800

    changed models.py

--- apps/gameadmin/views.py
+++ apps/gameadmin/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from ..game.models import Users, Characters, Monsters, Rooms, Items, Traps, Treasure, Exits
 
 def index(request):
     return render(request, 'login/base.html')
