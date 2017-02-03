f5260a424ff5cc9005da9f75d568c8b0ebca7d33
TenchuHawke <hawke@unrestrainedgames.com>
2017-02-02 18:13:54 -0800

    still working on core logic

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^main', views.main, name='game_main'),
+    url(r'^start_combat', views.start_combat, name='game_combat'),
+    url(r'^$', views.index, name='game_index'),
 
 ]
--- apps/game/views.py
+++ apps/game/views.py
@@ -6,21 +6,32 @@ from random import randint
 
 
 def index(request):
+    return render(request, 'game/game.html')
+
+def main(request):
+    request.session['character_id']="3"
+    request.session['room_id']="2"
+    hero=request.session['character_id']
+    room=request.session['room_id']
     context = {
-    'users': Users.objects.all().order_by('username'),
-    'characters': Characters.objects.all().order_by('name'),
-    'monsters': Monsters.objects.all().order_by('name'),
-    'rooms': Rooms.objects.all().order_by('id'),
-    'items': Items.objects.all().order_by('name'),
-    'traps': Traps.objects.all().order_by('name'),
-    'treasures': Treasures.objects.all().order_by('name'),
-    'exits': Exits.objects.all(),
-    }
-    return render(request, 'game/base.html', context)
+    # 'users': Users.objects.filter(id=request.session['']).order_by('username'),
+        'location': Rooms.objects.get(id=room),
+        'hero': Characters.objects.get(id=hero),
+        'characters': Characters.objects.filter(populating__id=room).order_by('name'),
+        'characters': Characters.objects.filter(populating__id=room).filter(notkilled_by__=None).order_by('name'),
+        'monsters': Monsters.objects.filter(denizen__id=room).order_by('name').exclude(killed_by__id=hero),
+        'monsters_dead': Monsters.objects.filter(denizen__id=room).order_by('name').filter(killed_by__id=hero),
+        'peeks': Rooms.objects.filter(exits__comes_from__id=room).order_by('direction'),
+        'items': Items.objects.filter(owned_by__id=hero).order_by('name'),
+        'traps': Traps.objects.filter(dangers__id=room).order_by('name'),
+        'treasures': Treasures.objects.filter(reward__id=room).order_by('name'),
+        'exits': Exits.objects.filter(leads_to__id=room),
+        'exits': Exits.objects.filter(leads_to__id=room),
+        'rooms': Rooms.objects.all(),
+        }
+    return render(request, 'game/main.html', context)
 
 def start_combat(request):
-    request.session['character_id']="1",
-    request.session['room_id']="2",
     hero=Characters.objects.get(id=request.session['character_id']),
     room=Rooms.objects.get(id=request.session['room_id']),
     monsters=Monster.ojbects.filter(Monsters__populating_id=request.session['room_id'])
